1d3312ef3a3016ec1e9306f0d03ae25f27b8b272
aminb00 <aminborqal10@gmail.com>
2024-04-16 11:09:23 +0200

    Update CH4 ITALIA 2020

--- Demo/CH4_ITALIA.R
+++ Demo/CH4_ITALIA.R
@@ -15,7 +15,7 @@ lat <- ncvar_get(nc, "lat")
 lon_idx <- which(lon >= boundary[1] & lon <= boundary[2])
 lat_idx <- which(lat >= boundary[3] & lat <= boundary[4])
 
-# Extract data for the defined region
+# Extract data for Italy
 data <- ncvar_get(nc, "SumAllSectors",
   start = c(min(lon_idx), min(lat_idx), 1),
   count = c(length(lon_idx), length(lat_idx), nc$dim$time$len)
@@ -23,22 +23,21 @@ data <- ncvar_get(nc, "SumAllSectors",
 
 # Create a data frame
 df <- expand.grid(x = lon[lon_idx], y = lat[lat_idx])
-df$value <- as.vector(data[, , 1]) # Assuming you're interested in data for a specific year (first time step)
+df$value <- as.vector(data[, , 1])
 
 # Plot the data using ggplot2
 p <- ggplot(df, aes(x = x, y = y, fill = value)) +
   geom_tile() + # This creates a grid-based heatmap
-  scale_fill_viridis_c(option = "C", name = "Emissioni (kg/m²)") + # Sets a color scale and labels the legend
-  coord_fixed(1.3) + # This helps maintain the aspect ratio, adjust as necessary for your display
-  theme_minimal() + # Uses a minimal theme for a cleaner look
-  theme(legend.position = "bottom") + # Positions the legend at the bottom
+  scale_fill_viridis_c(option = "C", name = "Emissioni (kg/m²)") + # Using viridis color scale
+  coord_fixed(1.3) +
+  theme_minimal() +
+  theme(legend.position = "bottom") +
   labs(
     title = "Emissioni di Metano in Italia nel 2020",
     subtitle = "Analisi distributiva regionale delle emissioni",
     x = "Longitudine",
     y = "Latitudine",
     caption = "Fonte: CAMS-REG-ANT EUR 0.05x0.1 anthro CH4 v5.1 yearly"
-  ) # Adds a source caption
+  )
 
-# Print the plot
 print(p)

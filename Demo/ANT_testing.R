dd76808034dd885910882f58bedc3951d9e9836f
aminb00 <aminborqal10@gmail.com>
2024-08-19 14:06:19 +0200

    new matrix for CAMS-ANT yearly data instead of a list

---
+++ Demo/ANT_testing.R
@@ -0,0 +1,15 @@
+library(ncdf4)
+library(abind)
+
+source("C:\\Users\\aminb\\Desktop\\IEDD\\Demo\\ANT\\\Utils.R")
+source("C:\\Users\\aminb\\Desktop\\IEDD\\Demo\\ANT\\Config.R")
+
+
+v<-c(1,2,3,4)
+
+
+z<-all_years_data[[1]]
+matrice2000<-NULL
+
+
+ 
--- Demo/Main.R
+++ Demo/Main.R
@@ -1,8 +1,8 @@
 
 #CAMS-REG-ANT
 
-source("Code\\Demo\\ANT\\Analysis.R")
-all_years_data <- list()
+source("C:\\Users\\aminb\\Desktop\\IEDD\\Demo\\ANT\\Analysis.R")
+all_data_list <- list()
 
 nc_file_paths <- c(
     "C:\\Users\\aminb\\Desktop\\TesiBorqal\\Data\\Raw\\CAMS-REG-ANT\\CAMS-REG-ANT_EUR_0.05x0.1_anthro_nh3_v5.1_yearly.nc",
@@ -11,8 +11,45 @@ nc_file_paths <- c(
 )
 
 for (nc_file_path in nc_file_paths) {
-    all_years_data <- add_new_years_data(nc_file_path, all_years_data)
+    all_data_list <- add_new_years_data(nc_file_path, all_data_list)
 }
+
+#salviamo anno per anno i dati in una matrice
+
+all_data_matrix<-NULL
+
+for(year in 1:length(all_data_list)){
+  
+  year_matrix<-NULL
+  
+  for(sector in all_data_list[[year]])
+  {
+    
+    year_matrix<-abind(year_matrix,  dcast(sector, x ~ y, value.var = "value") ,along=3)
+  }
+  
+    all_data_matrix<-abind(all_data_matrix,year_matrix,along=4)
+  
+}
+
+rm(all_data_list,sector,year_matrix,year,nc_file_path,nc_file_paths)
+gc()
+
+
+#time series extraction
+time_series<-NULL
+for(i in 1:dim(all_data_matrix)[4]){
+  
+  time_series<-c(time_series,all_data_matrix[30,30,3,i])
+}
+
+print(time_series)
+
+time_series*10^12
+
+#plot time_series
+plot(time_series*10^12, type="l", col="blue", xlab="Time", ylab="NH3 (ppb)", main="NH3 Time Series")
+
 
 #CAMS-REG-TEMPO
 
@@ -26,9 +63,4 @@ FM_B <- extract_and_store_profiles_in_list(nc_file_path, "FM_B")
 
 df <- FD_C[[120]]  # Accesso al 120° giorno
 
-
-
-
-
-COMMIT PROVA
 
